d05ea66316ed4d74b72a0e30366930601873a9f4
orvergon <franciscojosedasnevesneto@gmail.com>
2021-09-26 02:04:21 -0400

    ApplicationServices agora herdam de BaseApplicationService
    Signed-off-by: orvergon <franciscojosedasnevesneto@gmail.com>

--- webpet-backend/src/Pet.Application/Services/AnimalApplicationService.cs
+++ webpet-backend/src/Pet.Application/Services/AnimalApplicationService.cs
@@ -5,41 +5,10 @@ using Pet.Domain.Models;
 
 namespace Pet.Application.Services
 {
-    public class AnimalApplicationService : IAnimalApplicationService
+    public class AnimalApplicationService : BaseApplicationService<Animal>, IAnimalApplicationService
     {
-        private readonly IPetRepository _repository;
-        
-        public AnimalApplicationService()
-        { }
-        
-        public AnimalApplicationService(IPetRepository repository)
+        public AnimalApplicationService(IBaseRepository<Animal> repository) : base(repository)
         {
-            _repository = repository;
-        }
-
-        public void Add(Animal animal)
-        {
-            _repository.Add(animal);
-        }
-
-        public void Update(Animal animal)
-        {
-            _repository.Update(animal);
-        }
-
-        public void Remove(Animal animal)
-        {
-            _repository.Remove(animal);
-        }
-
-        public IEnumerable<Animal> GetAll()
-        {
-            return _repository.GetAll();
-        }
-
-        public Animal GetById(int id)
-        {
-            return _repository.GetById(id);;
         }
     }
 }
--- webpet-backend/src/Pet.Application/Services/CustomerApplicationService.cs
+++ webpet-backend/src/Pet.Application/Services/CustomerApplicationService.cs
@@ -5,38 +5,12 @@ using Pet.Domain.Models;
 
 namespace Pet.Application.Services
 {
-    public class CustomerApplicationService : ICustomerApplicationService
+    public class CustomerApplicationService : BaseApplicationService<Customer>, ICustomerApplicationService
     {
         private readonly ICustomerRepository _repository;
-        
-        public CustomerApplicationService(ICustomerRepository repository)
-        {
-            _repository = repository;
-        }
-    
-        public void Add(Customer customer)
-        {   
-            _repository.Add(customer);
-        }
 
-        public void Update(Customer customer)
+        public CustomerApplicationService(IBaseRepository<Customer> repository) : base(repository)
         {
-            _repository.Update(customer);
-        }
-
-        public void Remove(Customer customer)
-        {
-            _repository.Remove(customer);
-        }
-
-        public IEnumerable<Customer> GetAll()
-        {
-            return _repository.GetAll();
-        }
-
-        public Customer GetById(int id)
-        {
-            return _repository.GetById(id);
         }
     }
 }
